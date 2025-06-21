7c10d8976a685352f96e6818212ae3e4911d89de
Yinyue93 <cristopherbonn4@gmail.com>
2025-06-21 16:25:45 -0300

    Live radio

--- public/js/main.js
+++ public/js/main.js
@@ -4,14 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const stopBtn = document.getElementById('stopBtn');
 
     playBtn.addEventListener('click', () => {
-        if (audioPlayer.src !== window.location.origin + '/stream') {
-            audioPlayer.src = '/stream';
+        // Only do something if the player is paused. This prevents
+        // restarting the stream if the play button is clicked again.
+        if (audioPlayer.paused) {
+            // If the player doesn't have a source yet, this is the very first play.
+            if (!audioPlayer.currentSrc) {
+                audioPlayer.src = '/stream';
+            } else {
+                // If it has a source, it means we were paused.
+                // Reload the stream to fetch the live broadcast.
+                audioPlayer.load();
+            }
+            audioPlayer.play();
         }
-        audioPlayer.play();
     });
 
     stopBtn.addEventListener('click', () => {
         audioPlayer.pause();
-        // We no longer set audioPlayer.src = '' to allow for pausing and resuming.
     });
 }); 
